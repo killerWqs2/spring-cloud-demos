d1a10b6a0c09f6e906ad52de4cd026ff81bcb896
指尖上码农 <killerWqs@gmail.com>
2020-07-09 18:05:19 +0800

    update

---
+++ spring-cloud-dubbo/src/test/java/org/killer/springclouddubbo/unit/DubboSpiTest.java
@@ -0,0 +1,20 @@
+package org.killer.springclouddubbo.unit;
+
+import org.apache.dubbo.common.extension.ExtensionLoader;
+import org.junit.jupiter.api.Test;
+
+import java.security.cert.Extension;
+
+/**
+ * @author wqs
+ * @date 2020/7/9-17:29
+ * @description
+ */
+public class DubboSpiTest {
+
+    @Test
+    public void test() {
+        ExtensionLoader.getExtensionLoader()
+    }
+
+}
