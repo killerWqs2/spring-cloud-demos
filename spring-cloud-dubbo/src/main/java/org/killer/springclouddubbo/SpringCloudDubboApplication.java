e356084b67cb3ca06ab3f154210a0d3ec1b9003f
指尖上码农 <killerWqs@gmail.com>
2020-07-09 16:32:39 +0800

    submit spring-cloud-dubbo module

---
+++ spring-cloud-dubbo/src/main/java/org/killer/springclouddubbo/SpringCloudDubboApplication.java
@@ -0,0 +1,13 @@
+package org.killer.springclouddubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudDubboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudDubboApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-dubbo/src/test/java/org/killer/springclouddubbo/SpringCloudDubboApplicationTests.java
@@ -0,0 +1,13 @@
+package org.killer.springclouddubbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudDubboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
