bc2910d7afcd7b6fb0a125638573d3fa5b6238dc
指尖上码农 <killerWqs@gmail.com>
2020-07-13 17:42:24 +0800

    update

---
+++ spring-cloud-config/spring-cloud-config-client/src/test/java/org/killer/springcloudconfigclient/SpringCloudConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.killer.springcloudconfigclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
