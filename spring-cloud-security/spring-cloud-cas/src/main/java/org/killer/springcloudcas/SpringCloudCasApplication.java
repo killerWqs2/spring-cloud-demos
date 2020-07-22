481dfeb7c6acb5d355b91f9acd2c6e7d49b6301a
killerWqs <1249476676@qq.com>
2020-07-23 00:44:03 +0800

    update

---
+++ spring-cloud-security/spring-cloud-cas/src/main/java/org/killer/springcloudcas/SpringCloudCasApplication.java
@@ -0,0 +1,13 @@
+package org.killer.springcloudcas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudCasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudCasApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-security/spring-cloud-cas/src/test/java/org/killer/springcloudcas/SpringCloudCasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.killer.springcloudcas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudCasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
