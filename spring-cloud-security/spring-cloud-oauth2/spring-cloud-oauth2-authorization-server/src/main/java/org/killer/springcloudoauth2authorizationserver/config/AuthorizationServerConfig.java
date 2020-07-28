5380dc00d912cdb8f6ac51f898feea0bd6545b72
指尖上码农 <killerWqs@gmail.com>
2020-07-28 15:39:36 +0800

    update

--- spring-cloud-security/spring-cloud-oauth2/spring-cloud-oauth2-authorization-server/src/main/java/org/killer/springcloudoauth2authorizationserver/config/AuthorizationServerConfig.java
+++ spring-cloud-security/spring-cloud-oauth2/spring-cloud-oauth2-authorization-server/src/main/java/org/killer/springcloudoauth2authorizationserver/config/AuthorizationServerConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.code.InMemoryAuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
@@ -32,6 +33,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         super.configure(security);
+
         // security.authenticationEntryPoint() 用来exception 跳转
         security.allowFormAuthenticationForClients() // 这一步允许从requestParams中获取client-id, client-secret，设置校验client的功能
                 .tokenKeyAccess("permitAll()");
@@ -40,37 +42,50 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
         // 内置一个 client， 应用吧, oauth2 rfc文档就将这个称之为client，另外就是 authorizatin-server, resource-server
         clients.inMemory().withClient("test")
                 // 客户端模式
                 .secret("{noop}$2a$10$J9fGzwblYSR0TxeBNjtJC.wCyhN4cSnEHaEqYFQsdKVR7K5fWk0tu")
                 .authorizedGrantTypes("client_credentials")
                 .scopes("all")
+                // 自动授权，也可以用户手动同意，，
+                .autoApprove(true)
                 .and()
                 // 授权码模式
                 .withClient("t0")
                 .secret("{noop}wqsqzj")
                 // 用于认证完之后跳转回自己系统的连接
-                .redirectUris("")
+                .redirectUris("http://localhost:8082/test")
                 .authorizedGrantTypes("authorization_code")
-                .scopes("all");
+                .scopes("all")
+                .autoApprove(true);
 
         // oauth2要实现sso就是需要将第三方登录默认为自己的登录方式，，
+
     }
     // 感觉还是很复杂的，,
-
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         super.configure(endpoints);
+
+        // token 存在redis里面
         endpoints.tokenStore(new RedisTokenStore(lettuceConnectionFactory()));
 
-        endpoints.authorizationCodeServices(jdbcAuthorizationCodeServices());
+        // authorization code 存在内存里面， 这里存内存里面吧，公司电脑没有mysql
+        endpoints.authorizationCodeServices(inMemoryAuthorizationCodeServices());
+
     }
 
     @Bean
     public JdbcAuthorizationCodeServices jdbcAuthorizationCodeServices() {
         return new JdbcAuthorizationCodeServices(dataSource);
+    }
+
+    @Bean
+    public InMemoryAuthorizationCodeServices inMemoryAuthorizationCodeServices() {
+        return new InMemoryAuthorizationCodeServices();
     }
 
     @Bean
--- spring-cloud-security/spring-cloud-oauth2/spring-cloud-oauth2-client-server/src/main/java/org/killer/springcloudoauth2server/config/OAuth2ClientConfig.java
+++ spring-cloud-security/spring-cloud-oauth2/spring-cloud-oauth2-client-server/src/main/java/org/killer/springcloudoauth2server/config/OAuth2ClientConfig.java
@@ -11,17 +11,19 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  * @date 2020/07/26 - 22:32
  */
 @Configuration
-@EnableOAuth2Client
+//@EnableOAuth2Client
 @EnableOAuth2Sso // 这个需要配合oauth2Client使用
 public class OAuth2ClientConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // 开启oauth2 授权码模式
+        // 开启oauth2 授权码模式, 这个是没有办法跟Sso公用的，这里他持久化了开始的请求
         http.oauth2Client().and().authorizeRequests().anyRequest().authenticated();
         // 现在的问题是 我获取到token之后，如何登录呢？？
 
         // enableoauth2sso 用来校验oauth2 token的，，听有趣的，，
+        // 问题是权限呢，，有了token可以向权限服务器请求数据，， 这种架构是真的有问题，，
+
 
     }
 
