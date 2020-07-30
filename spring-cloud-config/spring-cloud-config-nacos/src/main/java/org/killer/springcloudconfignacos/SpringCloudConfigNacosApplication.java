c0ecb5be8f546acdcc3cce44f751ef5ba208b902
指尖上码农 <killerWqs@gmail.com>
2020-07-30 17:55:37 +0800

    update

---
+++ spring-cloud-config/spring-cloud-config-nacos/src/main/java/org/killer/springcloudconfignacos/SpringCloudConfigNacosApplication.java
@@ -0,0 +1,13 @@
+package org.killer.springcloudconfignacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigNacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigNacosApplication.class, args);
+    }
+
+}
--- spring-cloud-config/spring-cloud-config-server/src/main/java/org/killer/springcloudconfigserver/modules/test/TestController.java
+++ spring-cloud-config/spring-cloud-config-server/src/main/java/org/killer/springcloudconfigserver/modules/test/TestController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- *
  * 没有动态刷新，烦的一批
  *
  * @author wqs
