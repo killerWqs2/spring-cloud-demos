ac18e9b941c8242ed3c6976746857e8b2c313a43
指尖上码农 <killerWqs@gmail.com>
2020-07-30 15:46:44 +0800

    update

--- spring-cloud-rpc/spring-cloud-feign/src/main/java/org/killer/springcloudfeign/utils/rpc/UserService.java
+++ spring-cloud-rpc/spring-cloud-feign/src/main/java/org/killer/springcloudfeign/utils/rpc/UserService.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(url = "http://localhost:8080")
 //没有办法通过微服务名获取到地址，，这个时很关键的
-@FeignClient(name = "user-service")
+@FeignClient(value = "user-service")
 public interface UserService {
 
     @GetMapping("/user/{uid}")
