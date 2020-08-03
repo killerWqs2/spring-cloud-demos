6fa686e196e1f3af00dfe27dd8392453b02b48ea
killerWqs <1249476676@qq.com>
2020-08-04 00:08:14 +0800

    完成单点登录

--- spring-cloud-security/spring-cloud-oauth2/spring-cloud-oauth2-resource-server/src/main/java/org/killer/springcloudoauth2resourceserver/modules/user/controller/UserController.java
+++ spring-cloud-security/spring-cloud-oauth2/spring-cloud-oauth2-resource-server/src/main/java/org/killer/springcloudoauth2resourceserver/modules/user/controller/UserController.java
@@ -1,7 +1,9 @@
 package org.killer.springcloudoauth2resourceserver.modules.user.controller;
 
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,14 +18,15 @@ import java.util.Map;
 @RequestMapping("user")
 public class UserController {
 
-    @GetMapping("info")
+    @PostMapping("info")
     public Map<String, Object> getUserInfo() {
         // 问题是怎么获取用户信息呢？
         HashMap<String, Object> result = new HashMap<>();
-        result.put("username", "killerWqs");
-        result.put("authorities", new String[]{"all"});
-        // 获取client_id
-        Object details = SecurityContextHolder.getContext().getAuthentication().getDetails();
+        result.put("user_name", SecurityContextHolder.getContext().getAuthentication().getName());
+        result.put("authorities", SecurityContextHolder.getContext().getAuthentication().getAuthorities());
+        // 获取client_id,, 好像没有办法获取到
+        result.put("client_id", ((OAuth2Authentication)(SecurityContextHolder.getContext().getAuthentication())).getOAuth2Request().getClientId());
+        result.put("active", true);
         return result;
     }
 
